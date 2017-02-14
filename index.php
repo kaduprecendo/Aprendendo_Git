1e21f0cc3d171a3d080d374b09ddee388b5eb56a
kaduprecendo <kaduprecendo@gmail.com>
2017-02-14 20:07:21 -0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<title>Edukadu</title>
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	<header>
+		<a href="home.php">
+			<h1>EDUKADU</h1>
+		</a>
+			<p>Soluções simples para seu negócio</p>
+	</header>
+	
+</body>
+</html>
